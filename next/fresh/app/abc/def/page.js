b0ec19d11d1b97d2d4c2a37a576f8dfbf41650c9
chuchun4999 <117504584+chuchun4999@users.noreply.github.com>
2024-01-02 14:30:06 +0000

    next vue 1차끝

--- next/fresh/app/abc/def/page.js
+++
@@ -1,7 +0,0 @@
-export default function Def(){
-    return (
-        <div>
-            <h4>def입니다</h4>
-        </div>
-    )
-}
--- next/fresh/app/list/page.js
+++ next/fresh/app/list/page.js
@@ -1,3 +1,5 @@
+import Image from "next/image"
+
 export default function List() {
     let 상품 = ['Tomatoes', 'Pasta', 'Coconut']
     return (
@@ -6,8 +8,8 @@ export default function List() {
         {
           상품.map((n,i)=>{
             return(
-              <div className="food">
-                <img></img>
+              <div className="food" key={i}>
+                <img src={`/food${i}.png`} className="food-img"/>
                 <h4>{상품[i]} $40</h4>
               </div>
             )
