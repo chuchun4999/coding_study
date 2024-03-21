3fcf0ab6896e4249f61a2887a8f981573664f835
chuchun4999 <chuchun4999@naver.com>
2024-03-21 15:31:38 +0900

    selenium add

--- webcrawler/INSTAGRAM/blog.py
+++ webcrawler/INSTAGRAM/blog.py
@@ -4,24 +4,25 @@ import time
 import pyperclip
 
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
 
 옵션 = webdriver.ChromeOptions()
 # chrome://version 입력해서 프로필 경로 가져와서 복붙
-옵션.add_argument('user-data-dir=프로필경로') 
-driver = webdriver.Chrome(chrome_options:옵션)
+옵션.add_argument(r'user-data-dir=C:\Users\jisung\AppData\Local\Google\Chrome\User Data\Default') 
+driver = webdriver.Chrome()
 driver.get('https://nid.naver.com/nidlogin.login?svctype=262144')
 
 time.sleep(2)
 
 pyperclip.copy('chuchun4999')
-e = driver.find_element_by_css_selector('#id')
+e = driver.find_element(By.CSS_SELECTOR,'#id')
 e.send_keys(Keys.CONTROL, 'v')
 
 time.sleep(1)
 
 pyperclip.copy('비번')
-e = driver.find_element_by_css_selector('#pw')
+e = driver.find_element(By.CSS_SELECTOR,'#pw')
 e.send_keys(Keys.CONTROL, 'v')
 
 time.sleep(1)
-e.send_keys()
+e.send_keys(Keys.ENTER)
